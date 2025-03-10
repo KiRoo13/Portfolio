302eb1feef43871e2fbfdcfd0b807625dc7745da
Kirill_13 <k1rillrodionov@yandex.ru>
2025-03-10 12:59:19 +0300

    added origin photos

--- src/components/ProjectItem/ProjectItem.jsx
+++ src/components/ProjectItem/ProjectItem.jsx
@@ -1,14 +1,14 @@
 import { NavLink } from "react-router-dom";
-import './ProjectItem.css'
-
+import "./ProjectItem.css";
 
 function ProjectItem({ project }) {
-
   return (
     <NavLink to={`/projects/${project.id}`}>
       <div className="project-item">
         <div className="project-item__header">
-          <img src={project.img} alt="#" />
+          <div className="project-item-img">
+            <img src={project.img} alt="#" />
+          </div>
         </div>
         <div className="project-item__footer">
           <p>{project.title}</p>
--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,12 +1,16 @@
+import react1 from '../img/react1.webp'
+import react2 from '../img/react2.webp'
+import react3 from '../img/react3.webp'
 import img1 from '../img/project.webp'
 
 
 
+
 export const projectsList = [
-  { id: 1, img: img1, title: 'lorem1', category: 'React' },
+  { id: 1, img: react1, title: 'lorem1', category: 'React' },
   { id: 2, img: img1, title: 'lorem2', category: 'JS' },
-  { id: 3, img: img1, title: 'lorem3', category: 'React' },
+  { id: 3, img: react2, title: 'lorem3', category: 'React' },
   { id: 4, img: img1, title: 'lorem4', category: 'JS' },
   { id: 5, img: img1, title: 'lorem5', category: 'JS' },
-  { id: 6, img: img1, title: 'lorem6', category: 'React' },
+  { id: 6, img: react3, title: 'lorem6', category: 'React' },
 ];
